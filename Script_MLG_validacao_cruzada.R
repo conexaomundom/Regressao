42e3c1127f36edebd00248f3875f22991e16a74e
conexaomundom <48895083+conexaomundom@users.noreply.github.com>
2019-04-13 16:30:27 -0300

    Add files via upload
    Todas as possíveis dist. com as possíveis funções de ligação para a regresão com as variáveis.

--- Script_MLG_validacao_cruzada.R
+++ Script_MLG_validacao_cruzada.R
@@ -21,3 +21,100 @@ names(banco)
 
 # Rodar modelos MGL's e ver qual eh melhor por validacao cruzada.
 
+# modelo gaussiano com função de ligação identidade.
+m_g <- glm(formula = Percent ~ Age + Weigth + Heigth + Neck + Chest + 
+            Abdomen + Hip + Thigh + Knee + Ankle + Biceps + Forearm + 
+            Wrist, family = gaussian(link = "identity"), data=banco)
+
+# modelo gaussiano com função de ligação log.
+m_g <- glm(formula = Percent ~ Age + Weigth + Heigth + Neck + Chest + 
+             Abdomen + Hip + Thigh + Knee + Ankle + Biceps + Forearm + 
+             Wrist, family = gaussian(link = "log"), data=banco)
+
+
+# modelo gaussiano com função de ligação inversa.
+m_g <- glm(formula = Percent ~ Age + Weigth + Heigth + Neck + Chest + 
+             Abdomen + Hip + Thigh + Knee + Ankle + Biceps + Forearm + 
+             Wrist, family = gaussian(link = "inverse"), data=banco)
+
+
+##
+# modelo Gamma com função de ligação inversa.
+m_g <- glm(formula = Percent ~ Age + Weigth + Heigth + Neck + Chest + 
+             Abdomen + Hip + Thigh + Knee + Ankle + Biceps + Forearm + 
+             Wrist, family = Gamma(link = "inverse"), data=banco)
+
+# modelo Gamma com função de ligação identidade.
+m_g <- glm(formula = Percent ~ Age + Weigth + Heigth + Neck + Chest + 
+             Abdomen + Hip + Thigh + Knee + Ankle + Biceps + Forearm + 
+             Wrist, family = Gamma(link = "identity"), data=banco)
+
+# modelo Gamma com função de ligação log.
+m_g <- glm(formula = Percent ~ Age + Weigth + Heigth + Neck + Chest + 
+             Abdomen + Hip + Thigh + Knee + Ankle + Biceps + Forearm + 
+             Wrist, family = Gamma(link = "log"), data=banco)
+
+##
+# modelo gaussiana  inversa com funcao de ligacao 1/mu^2
+m_ig_mu <- glm(formula = Percent ~ Age + Weigth + Heigth + Neck + Chest + 
+             Abdomen + Hip + Thigh + Knee + Ankle + Biceps + Forearm + 
+             Wrist, family = inverse.gaussian(link = "1/mu^2"), data=banco)
+
+# modelo gaussiana  inversa com funcao de ligacao inverse. 
+m_ig_inverse <- glm(formula = Percent ~ Age + Weigth + Heigth + Neck + Chest + 
+              Abdomen + Hip + Thigh + Knee + Ankle + Biceps + Forearm + 
+              Wrist, family = inverse.gaussian(link = "inverse"), data=banco)
+
+
+# modelo gaussiana  inversa com funcao de ligacao identity. 
+m_ig_inverse <- glm(formula = Percent ~ Age + Weigth + Heigth + Neck + Chest + 
+                      Abdomen + Hip + Thigh + Knee + Ankle + Biceps + Forearm + 
+                      Wrist, family = inverse.gaussian(link = "identity"), data=banco)
+
+# modelo gaussiana  inversa com funcao de ligacao log.
+m_ig_inverse <- glm(formula = Percent ~ Age + Weigth + Heigth + Neck + Chest + 
+                      Abdomen + Hip + Thigh + Knee + Ankle + Biceps + Forearm + 
+                      Wrist, family = inverse.gaussian(link = "log"), data=banco)
+
+##
+# modelo quasi com funcao de ligacao logit. 
+m_g <- glm(formula = Percent ~ Age + Weigth + Heigth + Neck + Chest + 
+             Abdomen + Hip + Thigh + Knee + Ankle + Biceps + Forearm + 
+             Wrist, family = quasi(link = "logit"), data=banco)
+
+# modelo quasi com funcao de ligacao probit. 
+m_g <- glm(formula = Percent ~ Age + Weigth + Heigth + Neck + Chest + 
+             Abdomen + Hip + Thigh + Knee + Ankle + Biceps + Forearm + 
+             Wrist, family = quasi(link = "probit"), data=banco)
+
+# modelo quasi com funcao de ligacao cloglog. 
+m_g <- glm(formula = Percent ~ Age + Weigth + Heigth + Neck + Chest + 
+             Abdomen + Hip + Thigh + Knee + Ankle + Biceps + Forearm + 
+             Wrist, family = quasi(link = "cloglog"), data=banco)
+
+# modelo quasi com funcao de ligacao identity. 
+m_g <- glm(formula = Percent ~ Age + Weigth + Heigth + Neck + Chest + 
+             Abdomen + Hip + Thigh + Knee + Ankle + Biceps + Forearm + 
+             Wrist, family = quasi(link = "identity"), data=banco)
+
+# modelo quasi com funcao de ligacao inverse. 
+vm_g <- glm(formula = Percent ~ Age + Weigth + Heigth + Neck + Chest + 
+              Abdomen + Hip + Thigh + Knee + Ankle + Biceps + Forearm + 
+              Wrist, family = quasi(link = "inverse"), data=banco)
+
+
+# modelo quasi com funcao de ligacao log. 
+m_g <- glm(formula = Percent ~ Age + Weigth + Heigth + Neck + Chest + 
+             Abdomen + Hip + Thigh + Knee + Ankle + Biceps + Forearm + 
+             Wrist, family = quasi(link = "log"), data=banco)
+
+# modelo quasi com funcao de ligacao 1/mu^2. 
+m_g <- glm(formula = Percent ~ Age + Weigth + Heigth + Neck + Chest + 
+             Abdomen + Hip + Thigh + Knee + Ankle + Biceps + Forearm + 
+             Wrist, family = quasi(link = "1/mu^2"), data=banco)
+
+
+# modelo quasi com funcao de ligacao sqrt. 
+m_g <- glm(formula = Percent ~ Age + Weigth + Heigth + Neck + Chest + 
+             Abdomen + Hip + Thigh + Knee + Ankle + Biceps + Forearm + 
+             Wrist, family = quasi(link = "sqrt"), data=banco)
