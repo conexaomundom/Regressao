d5e50248875902b3d7e45b52e5527f94a9b14eb3
conexaomundom <48895083+conexaomundom@users.noreply.github.com>
2019-04-13 12:38:19 -0300

    Organizando as dist. que podem ser usadas.

--- Script_MLG_validacao_cruzada.R
+++ Script_MLG_validacao_cruzada.R
@@ -9,5 +9,15 @@ head(banco)
 attach(banco)
 names(banco)
 
+
+# Como a variável resposta é continua e não é contagem, podemos usar 
+# as distribuições gaussiana, Gamma, inverse.gaussian
+# nao podemos usar binomial, quasibinomial, nem opisson ou quasipoisson
+# vamos fazer os modelos com todas as funções de ligação possível.
+
+
+# Primeiramente com todas as variáveis e em seguinda ir fazendo a seleção 
+# de variaveis.
+
 # Rodar modelos MGL's e ver qual eh melhor por validacao cruzada.
 
