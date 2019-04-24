be72e3f81a488363de179e313bb35616fe80b004
conexaomundom <48895083+conexaomundom@users.noreply.github.com>
2019-04-24 12:48:47 -0300

    Ajeitando as médias porque estamos mexendo com listas.

--- Comecano a validacao cruzada.R
+++ Comecano a validacao cruzada.R
@@ -59,10 +59,18 @@ coef8f <- m8f$coefficients
 # A minha repetição começa aqui.
 ################################################################
 k <- 10
-i <- 1 # for(i in 1:k){
+cv <- 10
+# Contador para rodar as repetições
+
+j <- 1 # for(j in 1:k){
+
+
+# Contador para rodar os folds
+
+i <- 1 # for(i in 1:cv){
 
 # Separando os 10 folds.
-require(caret); cv <- 10
+require(caret); 
 flds <- createDataPartition(Percent, times = cv, p = 0.2, list = TRUE)
 
 
@@ -95,90 +103,69 @@ teste9 <- banco[flds[[9]], ]
 teste10 <- banco[flds[[10]], ]
 mat_teste <- list(teste1, teste2, teste3, teste4, teste5, teste6, teste7, teste8, teste9, teste10)
 
-################################################################
-# Matriz que receberá as estimativas utilizando 
-################################################################
-mat_t1 <- matrix(0,nrow(teste1), cv)
-mat_t2 <- matrix(0,nrow(teste2), cv)
-mat_t3 <- matrix(0,nrow(teste3), cv)
-mat_t4 <- matrix(0,nrow(teste4), cv)
-mat_t5 <- matrix(0,nrow(teste5), cv)
-mat_t6 <- matrix(0,nrow(teste6), cv)
-mat_t7 <- matrix(0,nrow(teste7), cv)
-mat_t8 <- matrix(0,nrow(teste8), cv)
-mat_t9 <- matrix(0,nrow(teste9), cv)
-mat_t10 <- matrix(0,nrow(teste10), cv)
-
-rmse1 <- matrix(0,nrow(teste1),k*cv)
+# Matrizes que vao receber as méidas das estimativas de rood mean square error
+# a quantidade dessas matrizes 
+mrmse1 <- matrix(0,cv,k)
+mrmse2 <- matrix(0,cv,k)
+mrmse3 <- matrix(0,cv,k)
+mrmse4 <- matrix(0,cv,k)
+mrmse5 <- matrix(0,cv,k)
+mrmse6 <- matrix(0,cv,k)
+mrmse7 <- matrix(0,cv,k)
+mrmse8 <- matrix(0,cv,k)
 
 ################################################################
-# Modelo com gaussiana com função de ligação identidade.
+t1 <- NULL
 m1 <- glm(formula = Percent ~ Age + Weigth + Neck + Abdomen + Thigh + 
           Forearm + Wrist, family = gaussian(link = "identity"), data=mat_treino[[i]])
-mat_t1[ ,i] <- predict(m1,newdata=data.frame(mat_teste[[i]]), level = 0.95, interval="prediction") ## valor individual
-rmse1[ ,i] <-  sqrt(mean(teste1[1] - mat_t1[ ,i])^2)
+t1 <- predict(m1,newdata=data.frame(mat_teste[[i]]), level = 0.95, interval="prediction")
+mrmse1[i,j] <- sqrt(apply(teste1[1] - t1,2,"mean")^2)
 
-
-# Modelo com gaussiana com função de ligação log.
+t2 <- NULL
 m2 <- glm(formula = Percent ~ Age + Weigth +  Neck + Abdomen + Hip + Thigh +
           Forearm + Wrist, family = gaussian(link = "log"), data=mat_treino[[i]])
-mat_t2[ ,i] <- predict(m2,newdata=data.frame(mat_teste[[i]]), level = 0.95, interval="prediction") ## valor individual
-coef2 <- m2$coefficients
-rmse1[ ,i+cv] <-  sqrt(mean(teste2[1] - mat_t2[ ,i])^2)
+t2 <- predict(m2,newdata=data.frame(mat_teste[[i]]), level = 0.95, interval="prediction")
+mrmse2[i,j] <- sqrt(apply(teste2[1] - t2,2,"mean")^2)
 
-# Modelo com gaussiana com função de ligação inversa.
+t3 <- NULL
 m3 <- glm(formula = Percent ~ Weigth + Abdomen + Hip + Thigh + Knee + 
           Forearm + Wrist, family = gaussian(link = "inverse"), data=mat_treino[[i]])
-mat_t3[ ,i] <- predict(m3,newdata=data.frame(mat_teste[[i]]), level = 0.95, interval="prediction") ## valor individual
-rmse1[ ,i+2*cv] <-  sqrt(mean(teste3[1] - mat_t3[ ,i])^2)
+t3 <- predict(m3,newdata=data.frame(mat_teste[[i]]), level = 0.95, interval="prediction") 
+mrmse3[i,j] <- sqrt(apply(teste3[1] - t3,2,"mean")^2)
 
-
-
-# Modelo com Gamma com função de ligação inversa.
+t4 <- NULL
 m4 <- glm(formula = Percent ~ Age + Weigth + Abdomen + Hip + Thigh + Knee + 
           Forearm + Wrist, family = Gamma(link = "inverse"), data=mat_treino[[i]])
-mat_t4[ ,i] <- predict(m4,newdata=data.frame(mat_teste[[i]]), level = 0.95, interval="prediction") ## valor individual
-rmse1[ ,i+3*cv] <-  sqrt(mean(teste4[1] - mat_t4[ ,i])^2)
+t4 <- predict(m4,newdata=data.frame(mat_teste[[i]]), level = 0.95, interval="prediction")
+mrmse4[i,j] <- sqrt(apply(teste4[1] - t4,2,"mean")^2)
 
-
-
-# Modelo com Gamma com função de ligação identidade.
+t5 <- NULL
 m5 <- glm(formula = Percent ~ 0 + Age + Neck + Abdomen + Hip + Thigh + 
           Forearm + Wrist, family = Gamma(link = "identity"), data=mat_treino[[i]])
-mat_t5[ ,i] <- predict(m5,newdata=data.frame(mat_teste[[i]]), level = 0.95, interval="prediction") ## valor individual
-rmse1[ ,i+4*cv] <-  sqrt(mean(teste5[1] - mat_t5[ ,i])^2)
+t5 <- predict(m5,newdata=data.frame(mat_teste[[i]]), level = 0.95, interval="prediction")
+mrmse5[i,j] <- sqrt(apply(teste5[1] - t5,2,"mean")^2)
 
-
-
-
-
-# Modelo com inversa gaussiana com função de ligação 1/mu^2.
+t6 <- NULL
 m6 <- glm(formula = Percent ~ Weigth + Abdomen + Hip + Thigh + Knee + 
           Forearm , family = inverse.gaussian(link = "1/mu^2"), data=mat_treino[[i]])
-mat_t6[ ,i] <- predict(m6,newdata=data.frame(mat_teste[[i]]), level = 0.95, interval="prediction") ## valor individual
-rmse1[ ,i+5*cv] <-  sqrt(mean(teste6[1] - mat_t6[ ,i])^2)
+t6 <- predict(m6,newdata=data.frame(mat_teste[[i]]), level = 0.95, interval="prediction")
+mrmse6[i,j] <- sqrt(apply(teste6[1] - t6,2,"mean")^2)
 
-
-
-
-# Modelo com Gamma com função de ligação log.
+t7 <- NULL
 m7 <- glm(formula = Percent ~ 0 + Age + Weigth + Abdomen + Thigh + Knee + 
           Forearm + Wrist, family = Gamma(link = "log"), data=mat_treino[[i]])
-mat_t7[ ,i] <- predict(m7,newdata=data.frame(mat_teste[[i]]), level = 0.95, interval="prediction") ## valor individual
-rmse1[ ,i+6*cv] <-  sqrt(mean(teste7[1] - mat_t7[ ,i])^2)
+t7 <- predict(m7,newdata=data.frame(mat_teste[[i]]), level = 0.95, interval="prediction")
+mrmse7[i,j] <- sqrt(apply(teste7[1] - t7,2,"mean")^2)
 
-
-
-# Modelo com Gamma com função de ligação log.
+t8 <- NULL
 m8 <- glm(formula = Percent ~ Age + Weigth + Neck + Abdomen + Hip + Thigh +
           Forearm + Wrist, family = inverse.gaussian(link = "inverse"),
           data=mat_treino[[i]])
-mat_t8[ ,i] <- predict(m8,newdata=data.frame(mat_teste[[i]]), level = 0.95, interval="prediction") ## valor individual
-rmse1[ ,i+7*cv] <-  sqrt(mean(teste8[1] - mat_t8[ ,i])^2)
-
-
+t8 <- predict(m8,newdata=data.frame(mat_teste[[i]]), level = 0.95, interval="prediction")
+mrmse8[i,j] <- sqrt(apply(teste8[1] - t8,2,"mean")^2)
 
 # }
 
+#}
 
 
