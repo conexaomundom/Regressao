929549d39ffa592cf44761c4635f812599c3866f
conexaomundom <48895083+conexaomundom@users.noreply.github.com>
2019-04-24 12:08:25 -0300

    Ajeitando os for's pra rodar a validação cruzada.

--- Comecano a validacao cruzada.R
+++ Comecano a validacao cruzada.R
@@ -17,7 +17,7 @@ banco$Percent <- banco$Percent + 10
 banco
 
 
-########################################################
+###############################################################
 # Modelos de betas originais.
 ################################################################
 # Modelo com gaussiana com função de ligação identidade.
@@ -55,13 +55,18 @@ m8f <- glm(formula = Percent ~ Age + Weigth + Neck + Abdomen + Hip + Thigh +
 coef8f <- m8f$coefficients
 
 
+################################################################
+# A minha repetição começa aqui.
+################################################################
+k <- 10
+i <- 1 # for(i in 1:k){
 
 # Separando os 10 folds.
-require(caret)
-k <- 10
-cv <- 10
+require(caret); cv <- 10
 flds <- createDataPartition(Percent, times = cv, p = 0.2, list = TRUE)
-names(flds)
+
+
+
 ################################################################
 # Lista com os elementos separados para treino.
 ################################################################
@@ -75,9 +80,7 @@ train7 <- banco[-flds[[7]], ]
 train8 <- banco[-flds[[8]], ]
 train9 <- banco[-flds[[9]], ]
 train10 <- banco[-flds[[10]], ]
-
 mat_treino <- list(train1, train2, train3, train4, train5, train6, train7, train8, train9, train10)
-
 
 # Lista com os elementos separados para teste.
 teste1 <- banco[flds[[1]], ]
@@ -90,44 +93,30 @@ teste7 <- banco[flds[[7]], ]
 teste8 <- banco[flds[[8]], ]
 teste9 <- banco[flds[[9]], ]
 teste10 <- banco[flds[[10]], ]
-
 mat_teste <- list(teste1, teste2, teste3, teste4, teste5, teste6, teste7, teste8, teste9, teste10)
 
 ################################################################
 # Matriz que receberá as estimativas utilizando 
 ################################################################
-mat_t1 <- matrix(0,nrow(teste1), k)
-mat_t2 <- matrix(0,nrow(teste2), k)
-mat_t3 <- matrix(0,nrow(teste3), k)
-mat_t4 <- matrix(0,nrow(teste4), k)
-mat_t5 <- matrix(0,nrow(teste5), k)
-mat_t6 <- matrix(0,nrow(teste6), k)
-mat_t7 <- matrix(0,nrow(teste7), k)
-mat_t8 <- matrix(0,nrow(teste8), k)
-mat_t9 <- matrix(0,nrow(teste9), k)
-mat_t10 <- matrix(0,nrow(teste10), k)
+mat_t1 <- matrix(0,nrow(teste1), cv)
+mat_t2 <- matrix(0,nrow(teste2), cv)
+mat_t3 <- matrix(0,nrow(teste3), cv)
+mat_t4 <- matrix(0,nrow(teste4), cv)
+mat_t5 <- matrix(0,nrow(teste5), cv)
+mat_t6 <- matrix(0,nrow(teste6), cv)
+mat_t7 <- matrix(0,nrow(teste7), cv)
+mat_t8 <- matrix(0,nrow(teste8), cv)
+mat_t9 <- matrix(0,nrow(teste9), cv)
+mat_t10 <- matrix(0,nrow(teste10), cv)
 
-
-
-
-
-
-
-
-
-# for(i in 1:1){
-i <- 1
-
-
-
-
+rmse1 <- matrix(0,nrow(teste1),k*cv)
 
 ################################################################
 # Modelo com gaussiana com função de ligação identidade.
 m1 <- glm(formula = Percent ~ Age + Weigth + Neck + Abdomen + Thigh + 
           Forearm + Wrist, family = gaussian(link = "identity"), data=mat_treino[[i]])
 mat_t1[ ,i] <- predict(m1,newdata=data.frame(mat_teste[[i]]), level = 0.95, interval="prediction") ## valor individual
-rmse1 <-  (teste1[1] - mat_t1[ ,i])^2
+rmse1[ ,i] <-  sqrt(mean(teste1[1] - mat_t1[ ,i])^2)
 
 
 # Modelo com gaussiana com função de ligação log.
@@ -135,13 +124,13 @@ m2 <- glm(formula = Percent ~ Age + Weigth +  Neck + Abdomen + Hip + Thigh +
           Forearm + Wrist, family = gaussian(link = "log"), data=mat_treino[[i]])
 mat_t2[ ,i] <- predict(m2,newdata=data.frame(mat_teste[[i]]), level = 0.95, interval="prediction") ## valor individual
 coef2 <- m2$coefficients
-
+rmse1[ ,i+cv] <-  sqrt(mean(teste2[1] - mat_t2[ ,i])^2)
 
 # Modelo com gaussiana com função de ligação inversa.
 m3 <- glm(formula = Percent ~ Weigth + Abdomen + Hip + Thigh + Knee + 
           Forearm + Wrist, family = gaussian(link = "inverse"), data=mat_treino[[i]])
 mat_t3[ ,i] <- predict(m3,newdata=data.frame(mat_teste[[i]]), level = 0.95, interval="prediction") ## valor individual
-coef3 <- m3$coefficients
+rmse1[ ,i+2*cv] <-  sqrt(mean(teste3[1] - mat_t3[ ,i])^2)
 
 
 
@@ -149,7 +138,7 @@ coef3 <- m3$coefficients
 m4 <- glm(formula = Percent ~ Age + Weigth + Abdomen + Hip + Thigh + Knee + 
           Forearm + Wrist, family = Gamma(link = "inverse"), data=mat_treino[[i]])
 mat_t4[ ,i] <- predict(m4,newdata=data.frame(mat_teste[[i]]), level = 0.95, interval="prediction") ## valor individual
-coef4 <- m4$coefficients
+rmse1[ ,i+3*cv] <-  sqrt(mean(teste4[1] - mat_t4[ ,i])^2)
 
 
 
@@ -157,7 +146,7 @@ coef4 <- m4$coefficients
 m5 <- glm(formula = Percent ~ 0 + Age + Neck + Abdomen + Hip + Thigh + 
           Forearm + Wrist, family = Gamma(link = "identity"), data=mat_treino[[i]])
 mat_t5[ ,i] <- predict(m5,newdata=data.frame(mat_teste[[i]]), level = 0.95, interval="prediction") ## valor individual
-coef5 <- m1$coefficients
+rmse1[ ,i+4*cv] <-  sqrt(mean(teste5[1] - mat_t5[ ,i])^2)
 
 
 
@@ -167,7 +156,7 @@ coef5 <- m1$coefficients
 m6 <- glm(formula = Percent ~ Weigth + Abdomen + Hip + Thigh + Knee + 
           Forearm , family = inverse.gaussian(link = "1/mu^2"), data=mat_treino[[i]])
 mat_t6[ ,i] <- predict(m6,newdata=data.frame(mat_teste[[i]]), level = 0.95, interval="prediction") ## valor individual
-coef6 <- m6$coefficients
+rmse1[ ,i+5*cv] <-  sqrt(mean(teste6[1] - mat_t6[ ,i])^2)
 
 
 
@@ -176,7 +165,7 @@ coef6 <- m6$coefficients
 m7 <- glm(formula = Percent ~ 0 + Age + Weigth + Abdomen + Thigh + Knee + 
           Forearm + Wrist, family = Gamma(link = "log"), data=mat_treino[[i]])
 mat_t7[ ,i] <- predict(m7,newdata=data.frame(mat_teste[[i]]), level = 0.95, interval="prediction") ## valor individual
-coef7 <- m7$coefficients
+rmse1[ ,i+6*cv] <-  sqrt(mean(teste7[1] - mat_t7[ ,i])^2)
 
 
 
@@ -185,7 +174,7 @@ m8 <- glm(formula = Percent ~ Age + Weigth + Neck + Abdomen + Hip + Thigh +
           Forearm + Wrist, family = inverse.gaussian(link = "inverse"),
           data=mat_treino[[i]])
 mat_t8[ ,i] <- predict(m8,newdata=data.frame(mat_teste[[i]]), level = 0.95, interval="prediction") ## valor individual
-coef8 <- m8$coefficients
+rmse1[ ,i+7*cv] <-  sqrt(mean(teste8[1] - mat_t8[ ,i])^2)
 
 
 
